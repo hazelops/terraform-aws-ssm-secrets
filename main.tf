7b96771b1b046228a5bd04bc71d047a3741cf4ee
Dmitry Borisov <kobrikx@gmail.com>
2020-06-04 17:32:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,7 @@ resource "aws_ssm_parameter" "this" {
     EnvVarName  = each.value
   }
 }
+resource "aws_instance" "foo" {
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t1.2xlarge" # invalid type!
+}
